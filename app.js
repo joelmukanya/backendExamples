52ac311c12b3c6be0a1d0f956d92fe13ed1c2cbb
joelmukanya <joel@lifechoices.co.za>
2022-07-16 12:27:50 +0200

    Example 1

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
+// Importing module
+const os = require('os');
+const path = require('path')
 // Open your terminal and run npm app
 console.log('Hello There');
-
+// Example 1
+console.log('Global: ');
+console.log(global)
+console.log('__dirname & __filename');
+console.log(__dirname);
+console.log(__filename);
+// OS
+console.log('OS');
+console.log(os.type());
+console.log(os.version());
+console.log(os.homedir());
+console.log('path');
+console.log(path.extname(__filename));
+console.log(path.parse(__filename));
