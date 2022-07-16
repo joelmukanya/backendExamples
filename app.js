c12562115afb36c8128e0ec22e5bb0df93645cac
joelmukanya <joel@lifechoices.co.za>
2022-07-16 12:32:42 +0200

    Example 2

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 // Importing module
-const os = require('os');
-const path = require('path')
+// const os = require('os');
+// const path = require('path')
+const { addition } = require ('./myModule');
 // Open your terminal and run npm app
-console.log('Hello There');
+// console.log('Hello There');
 // Example 1
-console.log('Global: ');
-console.log(global)
-console.log('__dirname & __filename');
-console.log(__dirname);
-console.log(__filename);
+// console.log('Global: ');
+// console.log(global)
+// console.log('__dirname & __filename');
+// console.log(__dirname);
+// console.log(__filename);
 // OS
-console.log('OS');
-console.log(os.type());
-console.log(os.version());
-console.log(os.homedir());
-console.log('path');
-console.log(path.extname(__filename));
-console.log(path.parse(__filename));
+// console.log('OS');
+// console.log(os.type());
+// console.log(os.version());
+// console.log(os.homedir());
+// console.log('path');
+// console.log(path.extname(__filename));
+// console.log(path.parse(__filename));
+
+// Example 2
+console.log('Addition: ', addition(7, 9));
